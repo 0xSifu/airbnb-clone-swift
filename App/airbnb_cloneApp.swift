1be4c3098d6f919642f0e7e33c46d90144e91bf3
Lutfi Aldi Nugroho <lutfi.nugroho@acrossasiaassist.co.id>
2024-08-26 22:07:29 +0700

    initialize

---
+++ App/airbnb_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnb_cloneApp.swift
+//  airbnb-clone
+//
+//  Created by Lutfi Aldi Nugroho on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnb_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
